c0c9ff4055dc423d14bead3dbb0a3696a4c6d6d0
kenreurison <kenreurison@gmail.com>
2016-05-27 01:31:58 -0300

    Enviando o cliente.

--- src/servidorarquivos/Cliente.java
+++ src/servidorarquivos/Cliente.java
@@ -7,23 +7,25 @@ package servidorarquivos;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Amanda
  */
 public class Cliente {
-    Socket s_cliente;
 
-    public Cliente() {
+    public static void main(String[] args) {
+        Socket s_cliente;
         try {
-            this.s_cliente = new Socket("192.168.0.3", 12345);
+            s_cliente = new Socket("192.168.0.3", 12348);
             System.out.println("Conexion OK");
         } catch (IOException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("E agora jose?");
         }
     }
-            
+
+    public Cliente() {
+
+    }
+
 }
